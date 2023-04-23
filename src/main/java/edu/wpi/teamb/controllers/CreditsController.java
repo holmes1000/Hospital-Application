c568038928c143873d5596e0329cc21efb4cf2f7
JBalky <107571352+JBalky@users.noreply.github.com>
2023-04-23 15:34:51 -0400

    Started Credits page (Not done)

---
+++ src/main/java/edu/wpi/teamb/controllers/CreditsController.java
@@ -0,0 +1,58 @@
+package edu.wpi.teamb.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class CreditsController {
+
+    @FXML
+    private Label groundFloor;
+
+    @FXML private Label LL1;
+    @FXML private Label LL2;
+    @FXML private Label firstFloor;
+    @FXML private Label secondFloor;
+    @FXML private Label thirdFloor;
+    @FXML private Label breakfast;
+    @FXML private Label BWH_Background;
+    @FXML private Label BWH_Logo;
+    @FXML private Label BWH_Logo_2;
+    @FXML private Label Cog_Wheel;
+    @FXML private Label directions;
+    @FXML private Label flower;
+    @FXML private Label helpIcon;
+    @FXML private Label Home;
+    @FXML private Label logOut;
+    @FXML private Label map;
+    @FXML private Label mealTimes;
+    @FXML private Label phoneCall;
+    @FXML private Label reject;
+    @FXML private Label reply;
+    @FXML private Label send;
+    @FXML private Label sofa;
+    @FXML private Label street;
+    @FXML private Label workspace;
+
+
+
+    @FXML
+    private void handleLabelClick(ActionEvent event) {
+        // create a popup to show the image
+//        Image image = new Image("path/to/image.png"); // replace with the actual path to the image
+//        ImageView imageView = new ImageView(image);
+//        Popup popup = new Popup();
+//        popup.getContent().add(imageView);
+//
+//        // position the popup relative to the label
+//        double x = groundFloorLabel.getScene().getWindow().getX() + groundFloorLabel.getLayoutX() + groundFloorLabel.getWidth();
+//        double y = groundFloorLabel.getScene().getWindow().getY() + groundFloorLabel.getLayoutY();
+//        popup.setX(x);
+//        popup.setY(y);
+//
+//        // show the popup
+//        popup.show(groundFloorLabel.getScene().getWindow());
+    }
+}
