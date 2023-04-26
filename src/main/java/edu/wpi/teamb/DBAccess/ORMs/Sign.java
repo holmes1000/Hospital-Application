18ad25af3e7e3b185fabdd4ced5cc02ff0164543
Ronit Banerjee <ronitbandy@outlook.com>
2023-04-25 21:49:44 -0400

    added Sign backend stuff

---
+++ src/main/java/edu/wpi/teamb/DBAccess/ORMs/Sign.java
@@ -0,0 +1,102 @@
+package edu.wpi.teamb.DBAccess.ORMs;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class Sign {
+
+    private String signageGroup;
+    private String locationName;
+    private String direction;
+    private Date startDate;
+    private Date endDate;
+    private boolean singleBlock;
+
+    public Sign(){
+        this.signageGroup = "";
+        this.locationName = "";
+        this.direction = "stop here";
+        this.startDate = Date.valueOf(LocalDate.now());
+        this.endDate = null;
+        this.singleBlock = true;
+    }
+
+    public Sign(String signageGroup, String locationName, String direction, Date startDate, Date endDate, boolean singleBlock) {
+        this.signageGroup = signageGroup;
+        this.locationName = locationName;
+        this.direction = direction;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.singleBlock = singleBlock;
+    }
+
+    public Sign(ResultSet rs) throws SQLException {
+        this(
+                rs.getString("signageGroup"),
+                rs.getString("locationName"),
+                rs.getString("direction"),
+                rs.getDate("startDate"),
+                rs.getDate("endDate"),
+                rs.getBoolean("singleBlock")
+        );
+    }
+
+    public String getSignageGroup() {
+        return signageGroup;
+    }
+
+    public void setSignageGroup(String signageGroup) {
+        this.signageGroup = signageGroup;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isSingleBlock() {
+        return singleBlock;
+    }
+
+    public void setSingleBlock(boolean singleBlock) {
+        this.singleBlock = singleBlock;
+    }
+
+    public String toString() {
+        return "Signage Group: " + signageGroup + "\n" +
+                "Location Name: " + locationName + "\n" +
+                "Direction: " + direction + "\n" +
+                "Start Date: " + startDate + "\n" +
+                "End Date: " + endDate + "\n" +
+                "Single Block: " + singleBlock + "\n";
+    }
+}
