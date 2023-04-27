43591df92ada28533ecdc946ca6097e83ddf0f38
Samara Holmes <46973232+holmes1000@users.noreply.github.com>
2023-04-27 18:25:04 -0400

    changed map editor buttons

---
+++ src/main/java/edu/wpi/teamb/controllers/mapeditor/AlignNodesState.java
@@ -0,0 +1,8 @@
+package edu.wpi.teamb.controllers.mapeditor;
+
+public class AlignNodesState implements MapEditorState{
+    @Override
+    public void printStatus() {
+        System.out.println("Align Nodes State");
+    }
+}
