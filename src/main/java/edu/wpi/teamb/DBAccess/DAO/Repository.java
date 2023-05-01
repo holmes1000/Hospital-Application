6be1d204c43d6a307c7becc5eb3a47c0862488c5
Ella <128170991+DunneyBunny@users.noreply.github.com>
2023-05-01 16:51:20 -0400

    Changes by apeeriz

--- src/main/java/edu/wpi/teamb/DBAccess/DAO/Repository.java
+++ src/main/java/edu/wpi/teamb/DBAccess/DAO/Repository.java
@@ -1438,6 +1438,13 @@ public class Repository {
         return fmr;
     }
 
+    public ArrayList<MealRequest> getMealRequests() {
+        ArrayList<MealRequest> mr = mealRequestDAO.getAllHelper1();
+        dbConnection.closeDBconnection();
+        dbConnection.forceClose();
+        return mr;
+    }
+
     public void updateMealRequest(MealRequest mr) {
         mealRequestDAO.update(mr);
         dbConnection.closeDBconnection();
@@ -1619,6 +1626,12 @@ public class Repository {
         dbConnection.forceClose();
         return fcr;
     }
+    public ArrayList<FlowerRequest> getFlowerRequests() {
+        ArrayList<FlowerRequest> fcr = flowerRequestDAO.getAllHelper1();
+        dbConnection.closeDBconnection();
+        dbConnection.forceClose();
+        return fcr;
+    }
 
     public void updateFlowerRequest(FlowerRequest fr) {
         flowerRequestDAO.update(fr);
@@ -1695,6 +1708,13 @@ public class Repository {
         return fcr;
     }
 
+    public ArrayList<FurnitureRequest> getFurnitureRequests() {
+        ArrayList<FurnitureRequest> fr = furnitureRequestDAO.getAllHelper1();
+        dbConnection.closeDBconnection();
+        dbConnection.forceClose();
+        return fr;
+    }
+
     public void updateFurnitureRequest(FurnitureRequest fr) {
         furnitureRequestDAO.update(fr);
         dbConnection.closeDBconnection();
@@ -1733,6 +1753,13 @@ public class Repository {
         dbConnection.closeDBconnection();
         dbConnection.forceClose();
         return fcr;
+    }
+
+    public ArrayList<OfficeRequest> getOfficeRequests() {
+        ArrayList<OfficeRequest> or = officeRequestDAO.getAllHelper1();
+        dbConnection.closeDBconnection();
+        dbConnection.forceClose();
+        return or;
     }
 
     public Connection getConnection() {
