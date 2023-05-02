caaea3879f8b283ea3aab933071e6be9c2f36863
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2023-05-02 03:27:28 -0400

    furniture requests change status implemented

--- src/main/java/edu/wpi/teamb/controllers/requests/FurnitureRequestControllerI.java
+++ src/main/java/edu/wpi/teamb/controllers/requests/FurnitureRequestControllerI.java
@@ -55,6 +55,8 @@ public class FurnitureRequestControllerI implements IRequestController{
 
     @FXML private MFXFilterComboBox<String> cbLongName;
 
+    @FXML private MFXFilterComboBox<String> cbChangeStatus;
+
     private edu.wpi.teamb.entities.requests.EFurnitureRequest EFurnitureRequest;
 
     public FurnitureRequestControllerI() {
@@ -142,6 +144,9 @@ public class FurnitureRequestControllerI implements IRequestController{
         initComboBoxChangeListeners();
 
         txtFldNotes.setTooltip(new Tooltip("Enter any special instructions"));
+        ObservableList<String> statuses = FXCollections.observableArrayList("In-Progress", "Pending", "Completed");
+        Collections.sort(statuses);
+        cbChangeStatus.setItems(statuses);
     }
 
     private void initComboBoxChangeListeners() {
@@ -248,7 +253,8 @@ public class FurnitureRequestControllerI implements IRequestController{
                 || cdAvailableModels.getValue() == null
                 || cdAssembly.getValue() == null
                 || cbEmployeesToAssign.getValue() == null
-                || cbLongName.getValue() == null;
+                || cbLongName.getValue() == null
+                || cbChangeStatus.getSelectionModel() == null;
     }
 
     @Override
@@ -305,6 +311,8 @@ public class FurnitureRequestControllerI implements IRequestController{
         txtFldNotes.setText(fullFurnitureRequest.getNotes());
         cbEmployeesToAssign.getSelectionModel().selectItem(fullFurnitureRequest.getEmployee());
         cbLongName.getSelectionModel().selectItem(fullFurnitureRequest.getLocationName());
+        cbChangeStatus.setVisible(true);
+        cbChangeStatus.getSelectionModel().selectItem(fullFurnitureRequest.getRequestStatus());
 
         //set the submit button to say update
         btnSubmit.setText("Update");
@@ -319,6 +327,7 @@ public class FurnitureRequestControllerI implements IRequestController{
             fullFurnitureRequest.setNotes(txtFldNotes.getText());
             fullFurnitureRequest.setEmployee(cbEmployeesToAssign.getValue());
             fullFurnitureRequest.setLocationName(cbLongName.getValue());
+            fullFurnitureRequest.setRequestStatus(cbChangeStatus.getValue());
 
             //update the request
             EFurnitureRequest.updateFurnitureRequest(fullFurnitureRequest);
