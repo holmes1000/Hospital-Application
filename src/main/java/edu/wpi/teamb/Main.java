f30f8418d8d2a2da0ca1764f020fd20e1f9017e9
Ronit Banerjee <ronitbandy@outlook.com>
2023-05-03 19:09:23 -0400

    all CSV import/export functions finalized

--- src/main/java/edu/wpi/teamb/Main.java
+++ src/main/java/edu/wpi/teamb/Main.java
@@ -8,24 +8,10 @@ import org.postgresql.util.PSQLException;
 public class Main {
 
   public static void main(String[] args) {
-//    Repository.getRepository().importAlertsFromCSV("C:\\Users\\Ronit\\Desktop\\alerts.csv", 0);
 
     Repository.getRepository();
-
-//    Repository.getRepository().switchTo(1);
-//    Repository.getRepository().exportNodesToCSV("Nodes", 3);
-//    Repository.getRepository().exportEdgesToCSV("Edges", 3);
-//    Repository.getRepository().exportLocationNamesToCSV("LocationNames", 3);
-//    Repository.getRepository().exportMovesToCSV("Moves", 3);
-//    Repository.getRepository().exportRequestsToCSV("Requests", 3);
-//    Repository.getRepository().exportConferenceRequestsToCSV("ConferenceRequests", 3);
-//    Repository.getRepository().exportFlowerRequestsToCSV("FlowerRequests", 3);
-//    Repository.getRepository().exportFurnitureRequestsToCSV("FurnitureRequests", 3);
-//    Repository.getRepository().exportMealRequestsToCSV("MealRequests", 3);
-//    Repository.getRepository().exportOfficeRequestsToCSV("OfficeRequests", 3);
+    //Repository.getRepository().exportAllToCSVFiles(3);
 
     LauncherImpl.launchApplication(Bapp.class, CustomPreloader.class, args);
   }
-
-  // shortcut: psvm
 }
